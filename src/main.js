fba468f0be5b0efac862ead7d490de248a18ad71
Harol Danfer <harolrios.hr@gmail.com>
2025-04-27 10:49:41 -0500

    Add main file for survey processing

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// main.js
+import { leerJsonEncuesta } from "./fileReader.js";
+import { validarDatosEncuesta } from "./validation.js";
+import { calcularEdadPromedio, calcularRespuestas, calcularPorcentaje } from "./stats.js";
+import { formatearResultados } from "./formatter.js";
+import fs from "fs";
+
+/**
+ * Procesa las encuestas desde un archivo JSON y genera un archivo de resultados.
+ * @param {string} rutaArchivo - Ruta del archivo JSON con los datos de las encuestas.
+ */
+async function procesarEncuestas(rutaArchivo) {
+  try {
+    let datos = await leerJsonEncuesta(rutaArchivo);
+    const encuestasValidas = validarDatosEncuesta(datos);
+    let edadPromedio = calcularEdadPromedio(encuestasValidas);
+    let conteoRespuestas = calcularRespuestas(encuestasValidas);
+    let respuestasPorcentaje = calcularPorcentaje(conteoRespuestas, encuestasValidas.length);
+    const resumen = formatearResultados(datos, encuestasValidas, edadPromedio, respuestasPorcentaje)
+    await fs.promises.writeFile(
+        "src/resultados.json",
+        JSON.stringify(resumen, null, 2),
+        "utf-8"
+      );
+    console.log("Archivo guardado con éxito.")
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+procesarEncuestas("datos.json");
